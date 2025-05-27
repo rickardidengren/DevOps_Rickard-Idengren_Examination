0c1f1a524871ecb5bce1bfc30dd60af985d21629
Rickard Idengren <rickardi_90@hotmail.com>
2025-05-27 09:55:30 +0200

    changed csvtojson-script to update data.json instead. Deleated Profiles1.json

--- csvtojson.py
+++ csvtojson.py
@@ -2,13 +2,13 @@ import csv
 import json
 
 csvFile = 'Profiles1.csv'
-jsonFile = 'Profile1.json'
+jsonFile = 'data.json'
 
 with open('Profiles1.csv', mode='r', encoding='utf-8') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     data = list(csv_reader)
 
-with open ('Profiles1.json', mode='w', encoding='utf-8') as json_file:
+with open ('data.json', mode='w', encoding='utf-8') as json_file:
     json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 print(f"'{csvFile}' is converted to '{jsonFile}'")
